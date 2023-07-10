14491725a2e499f55c02da94b7eaed375559308c
MAYANK NARANG <mayank260202@gmail.com>
2023-07-10 23:58:45 +0530

    changes

--- server.js
+++ server.js
@@ -65,10 +65,10 @@ app.use("/api/issues", issueRouter);
 // Serve static assets if in production
 if (process.env.NODE_ENV === "production") {
   // Set static folder
-  app.use(express.static("client/build"));
+  app.use(express.static("client/public"));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "client", "public", "index.html"));
   });
 }
 
